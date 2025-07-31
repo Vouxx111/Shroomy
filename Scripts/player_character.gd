a16eae2144ccd85dabebd84453cdf738e9c25945
Vouxx <ejs28605@proton.me>
2025-07-31 08:44:47 -0400

    Character Rotation
    Character now rotates to the direction that you are moving

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 10
+@onready var player_character_model: Node3D = $"Player Character Model"
 
+const SPEED = 10
+var Health = 100
+var maxHealth = 100
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -13,8 +16,16 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.y * SPEED
+		
+		# Rotate the mesh to face movement direction
+		var target_rotation = atan2(-1 * (direction.y), direction.x)
+		player_character_model.rotation.y = lerp_angle(player_character_model.rotation.y, target_rotation, delta * 10.0)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	move_and_slide()
+
+func take_damage(damage: int):
+	Health -= damage
+	print (Health)
